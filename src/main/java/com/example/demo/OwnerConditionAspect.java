0d2277847eee6bdf1765f74cc1e7dc8610a738e0
George Suveti <george.suveti@irian.ro>
2019-02-28 15:43:33 +0200

    - owner aspect

---
+++ src/main/java/com/example/demo/OwnerConditionAspect.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.Annotation;
+
+@Aspect
+@Configuration
+public class OwnerConditionAspect {
+
+    @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
+    protected void restControllerMethods() {
+    }
+
+    @Pointcut("within(@org.springframework.data.rest.webmvc.RepositoryRestController *)")
+    protected void repositoryRestControllerMethods() {
+    }
+
+
+    @Around("restControllerMethods() || repositoryRestControllerMethods()")
+    public Object before(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        //get original args
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        //get all annotations for arguments
+        MethodSignature signature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = signature.getMethod().getName();
+        Class<?>[] parameterTypes = signature.getMethod().getParameterTypes();
+        Annotation[][] annotations;
+        try {
+            annotations = proceedingJoinPoint.getTarget().getClass().
+                    getMethod(methodName, parameterTypes).getParameterAnnotations();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return proceedingJoinPoint.proceed(args);
+    }
+
+}
