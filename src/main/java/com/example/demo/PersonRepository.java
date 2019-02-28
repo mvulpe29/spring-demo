c5128283f0c535b6d6cc717c3d4c8865713572af
George Suveti <george.suveti@irian.ro>
2019-02-28 16:34:58 +0200

    - @CrossOrigin for person repo

--- src/main/java/com/example/demo/PersonRepository.java
+++ src/main/java/com/example/demo/PersonRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import th.co.geniustree.springdata.jpa.repository.JpaSpecificationExecutorWithProjection;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends JpaRepository<Person, Long>, JpaSpecificationExecutor<Person>, QueryByExampleExecutor<Person>, JpaSpecificationExecutorWithProjection<Person> {
 
