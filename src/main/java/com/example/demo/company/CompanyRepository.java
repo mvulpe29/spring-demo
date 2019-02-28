6e339cd6dd204874909d19e6cdb7bd486df923db
George Suveti <george.suveti@irian.ro>
2019-02-28 15:42:05 +0200

    - rename search method in company repo

--- src/main/java/com/example/demo/company/CompanyRepository.java
+++ src/main/java/com/example/demo/company/CompanyRepository.java
@@ -1,17 +1,21 @@
 package com.example.demo.company;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 
 @RepositoryRestResource(collectionResourceRel = "companies", path = "companies")
 public interface CompanyRepository extends JpaRepository<Company, Long>, JpaSpecificationExecutor {
 
-    List<Company> findAllByNameContaining(@Param("name") String name);
+    @PreAuthorize("hasPermission(#owner, 'read')")
+    @RestResource(path = "nameContains", rel = "nameContains")
+    Page<Company> findAllByOwnerAndNameContaining(@Param("owner") String owner, @Param("name") String name, Pageable pageable);
 }
 
 
